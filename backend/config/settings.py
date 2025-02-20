e5c9c87889ad30b844c83b74e4d6d904d32ef5c2
Keith Paul Kato <paulkato460@yahoo.co.uk>
2025-02-20 17:58:10 +0300

    Add restframework to INSTALLED_APPS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    #'rest_framework',
+    'rest_framework',
     'config',
     'core',
     'core.auth',
