31130e17ced2969b1d112054ed9a0ff0f9b2d3c7
Asiimwe lsaac 24/U/03765/PS <asiimweisaac40@gmail.com>
2025-02-25 12:40:35 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import Dashboard from "./components/Dashboard";
+function App() {
+  return (
+    <>
+    <Header/>
+    <main className='flex space-between'>
+      <SideBar/>
+      <Dashboard/>
+    </main>
+    </>
+
+  );
+}
+
+export default App;
