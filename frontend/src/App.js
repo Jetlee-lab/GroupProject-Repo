de2759ec958315c60ec30c73521e64f8ed996a03
ASIIMWE ISAAC <asiimweisaac40@gmail.com>
2025-03-15 22:09:23 +1100

    we addded sign up and login in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Header from './components/Header';
-
-  function App(){
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import Dashboard from "./components/Dashboard";
+function App() {
   return (
     <>
-         
-      <Header/>
-    </> 
+    <Header/>
+    <main className='flex space-between'>
+      <SideBar/>
+      <Dashboard/>
+    </main>
+    </>
+
   );
 }
 
-
 export default App;
-
