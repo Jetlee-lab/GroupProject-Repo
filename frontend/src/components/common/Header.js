44b960b1f9d6a9a04ca65cd3cc062e147876bada
Seanice Nabasirye <seanicetess@gmail.com>
2025-03-20 13:43:40 +0300

    Updated changes for the Notifications and Reports Pages

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -7,7 +7,7 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 export default function header() {
   return (
     <> 
-      <div className='flex items-center justify-between p-4 bg-blue-600t ext-white shadow-2xl'>
+      <div className='flex items-center justify-between p-4 bg-blue-600 text-white shadow-2xl'>
         {/*Logo*/}
         <div className='flex items-center bg-blue-600'>
           <img src={logo} className="App-logo rounded-full" alt="logo" width={70} />
