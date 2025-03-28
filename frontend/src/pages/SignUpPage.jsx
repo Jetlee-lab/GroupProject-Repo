57cb0671cbffbb5397cce8946d3ea475a1f96adc
Seanice Nabasirye <seanicetess@gmail.com>
2025-03-28 09:20:54 +0300

    Modified signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,62 +1,133 @@
-import React from 'react'
-import AITS_Logo from '../components/images/logo2.jpg'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import AITS_Logo from '../components/images/logo2.jpg';
+import { Link } from 'react-router-dom';
 
 const SignUpPage = () => {
   return (
-    <div className="bg-sky-100 container flex justify-center items-center ">
+    <div className="bg-sky-100 flex h-screen">
       {/* Left: Image */}
-      <div className="w-1/2 h-screen hidden lg:block">
-        <img src={AITS_Logo} alt="AITS Logo" className="object-cover  h-full" />
+      <div className="w-1/2 h-full hidden lg:block">
+        <img
+          src={AITS_Logo}
+          alt="AITS Logo"
+          className="object-cover w-full h-full"
+        />
       </div>
 
       {/* Right: Sign Up Form */}
-      <div className="  p-5 w-full lg:w-1/2">
-        <h1 className="text-2xl font-semibold my-3 text-center">Sign Up</h1>
-        <form action="#" method="">
+      <div className="p-5 w-full lg:w-1/2 flex items-center justify-center">
+        <div className="w-full max-w-md">
+          <h1 className="text-2xl font-semibold mt-20 mb-6 text-center">Sign Up</h1>
+          <form action="#" method="">
+            {/* Full Name */}
+            <div className="mb-4">
+              <label htmlFor="fullName" className="block text-gray-600">
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="fullName"
+                name="fullName"
+                required
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                autoComplete="off"
+              />
+            </div>
 
-          {/* Full Name */}
-          <div className="mb-4">
-            <label htmlFor="fullName" className="block text-gray-600">Full Name</label>
-            <input type="text" id="fullName" name="fullName" required className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autoComplete="off" />
+            {/* Email Input */}
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-600">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                autoComplete="off"
+              />
+            </div>
+
+            {/* Phone Number */}
+            <div className="mb-4">
+              <label htmlFor="phone" className="block text-gray-600">
+                Phone Number
+              </label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                required
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                autoComplete="off"
+              />
+            </div>
+
+            {/* Password Input */}
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-gray-800">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                required
+                name="password"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                autoComplete="off"
+              />
+            </div>
+
+            {/* Confirm Password Input */}
+            <div className="mb-4">
+              <label htmlFor="confirmPassword" className="block text-gray-800">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="confirmPassword"
+                required
+                name="confirmPassword"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                autoComplete="off"
+              />
+            </div>
+
+            {/* Reference Number Input */}
+            <div className="mb-4">
+              <label htmlFor="referenceNumber" className="block text-gray-800">
+                Reference Number
+              </label>
+              <input
+                type="text"
+                id="referenceNumber"
+                name="referenceNumber"
+                required
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                autoComplete="off"
+              />
+            </div>
+
+            {/* Sign Up Button */}
+            <button
+              type="submit"
+              className="bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-md py-2 px-4 w-full"
+            >
+              Sign Up
+            </button>
+          </form>
+
+          {/* Log in Link */}
+          <div className="mt-6 text-green-500 text-center">
+            <Link to="/login" className="hover:underline">
+              Already have an account? Log In Here
+            </Link>
           </div>
-
-          {/* Email Input */}
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-600">Email</label>
-            <input type="email" id="email" name="email" required className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autoComplete="off" />
-          </div>
-
-          {/* Phone Number */}
-          <div className="mb-4">
-            <label htmlFor="phone" className="block text-gray-600">Phone Number</label>
-            <input type="tel" id="phone" name="phone" required className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autoComplete="off" />
-          </div>
-
-          {/* Password Input */}
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-800">Password</label>
-            <input type="password" id="password" required name="password" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autoComplete="off" />
-          </div>
-
-          {/* Confirm Password Input */}
-          <div className="mb-4">
-            <label htmlFor="confirmPassword" className="block text-gray-800">Confirm Password</label>
-            <input type="password" id="confirmPassword" required name="confirmPassword" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autoComplete="off" />
-          </div>
-
-          
-          {/* Sign Up Button */}
-          <button type="submit" className="bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-md py-2 px-4 w-full">Sign Up</button>
-        </form>
-
-        {/* Log in Link */}
-        <div className="mt-6 text-green-500 text-center">
-          <Link to="/login" className="hover:underline">Already have an account? Log In Here</Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
