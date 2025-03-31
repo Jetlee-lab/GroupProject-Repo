8e9cb53b8d588d265e2e633384dc23130243955b
Akandwanaho Jonan <akandwanahojonan256@gmail.com>
2025-03-31 13:31:16 +0300

    Update Home.jsx
    included Makerere in the welcome statement

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="bg-gray-100 min-h-screen p-4">
-      <h1 className="text-2xl font-bold mb-4">Welcome to AITS</h1>
+      <h1 className="text-2xl font-bold mb-4">Welcome to AITS  for Makerere</h1>
       <p className="text-gray-700">
         This is the Academic Issue Tracking System (AITS). Please navigate to the
         dashboard to log or track issues.
