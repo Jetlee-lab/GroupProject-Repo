5e97e1d9f1739123aa4204d59ff2f1cf05a88ae1
Seanice Nabasirye <seanicetess@gmail.com>
2025-04-03 00:51:03 +0300

    Updated sideber

--- frontend/src/components/common/SideBar.jsx
+++ frontend/src/components/common/SideBar.jsx
@@ -1,28 +1,70 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
-import SideBarItem from './SideBarItem'
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
 import HelpIcon from '@mui/icons-material/Help';
-import {Link} from 'react-router-dom';
+import SideBarItem from './SideBarItem';
 
-const SideBar = () => {
+const SideBar = ({ userRole }) => {
+  const renderItems = () => {
+    console.log("User Role in Sidebar:", userRole);
+    switch (userRole) {
+      case 'Student':
+        return (
+          <>
+            <Link to="/"><SideBarItem Icon={HomeIcon} text={'Home'} /></Link>
+            <Link to="/student-reports"><SideBarItem Icon={AssignmentIcon} text={'Reports'} /></Link>
+            <Link to="/notifications"><SideBarItem Icon={NotificationsIcon} text={'Notifications'} /></Link>
+            <Link to="/settings"><SideBarItem Icon={SettingsIcon} text={'Settings'} /></Link>
+            <Link to="/help"><SideBarItem Icon={HelpIcon} text={'Help & Support'} /></Link>
+            <SideBarItem Icon={LogoutIcon} text={'Logout'} />
+          </>
+        );
+      case 'Lecturer':
+        return (
+          <>
+            <Link to="/"><SideBarItem Icon={HomeIcon} text={'Home'} /></Link>
+            <Link to="/lecturer-reports"><SideBarItem Icon={AssignmentIcon} text={'Reports'} /></Link>
+            <Link to="/notifications"><SideBarItem Icon={NotificationsIcon} text={'Notifications'} /></Link>
+            <Link to="/settings"><SideBarItem Icon={SettingsIcon} text={'Settings'} /></Link>
+            <Link to="/help"><SideBarItem Icon={HelpIcon} text={'Help & Support'} /></Link>
+            <SideBarItem Icon={LogoutIcon} text={'Logout'} />
+          </>
+        );
+      case 'Registrar':
+        return (
+          <>
+            <Link to="/"><SideBarItem Icon={HomeIcon} text={'Home'} /></Link>
+            <Link to="/registrar-reports"><SideBarItem Icon={AssignmentIcon} text={'Reports'} /></Link>
+            <Link to="/notifications"><SideBarItem Icon={NotificationsIcon} text={'Notifications'} /></Link>
+            <Link to="/settings"><SideBarItem Icon={SettingsIcon} text={'Settings'} /></Link>
+            <Link to="/help"><SideBarItem Icon={HelpIcon} text={'Help & Support'} /></Link>
+            <SideBarItem Icon={LogoutIcon} text={'Logout'} />
+          </>
+        );
+      default:
+        return (
+          <>
+            <Link to="/"><SideBarItem Icon={HomeIcon} text={'Home'} /></Link>
+            
+            <Link to="/notifications"><SideBarItem Icon={NotificationsIcon} text={'Notifications'} /></Link>
+            <Link to="/settings"><SideBarItem Icon={SettingsIcon} text={'Settings'} /></Link>
+            <Link to="/help"><SideBarItem Icon={HelpIcon} text={'Help & Support'} /></Link>
+            <SideBarItem Icon={LogoutIcon} text={'Logout'} />
+          </>
+        );
+    }
+  };
+
   return (
-    <>
-      <div className='bg-green-100 border-r-2 border-b-2 rounded border-blue-600 overflow-y-auto row-start-2'>  
-      
-      <Link to="/"><SideBarItem Icon={HomeIcon} text={'Home'}/></Link>
-      <Link to="/registrar-reports"><SideBarItem Icon={AssignmentIcon} text={'Reports'}/></Link>
-      <Link to="/notifications"><SideBarItem Icon={NotificationsIcon} text={'Notifications'}/></Link>
-      <Link to="/settings"><SideBarItem Icon={SettingsIcon} text={'Settings'}/></Link>
-      <Link to="/help"><SideBarItem Icon={HelpIcon} text={'Help & Support'}/></Link>
-      <SideBarItem Icon={LogoutIcon} text={'Logout'}/>
+    <div className='bg-green-100 border-r-2 border-b-2 rounded border-blue-600 overflow-y-auto row-start-2'>
+      {renderItems()}
+    </div>
+  );
+};
 
-      </div>
-    </>
-  )
-}
+export default SideBar;
 
-export default SideBar
