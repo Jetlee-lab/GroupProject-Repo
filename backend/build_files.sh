51869d0980125a467e6c7ffcd4c315cb692095cb
Keith Paul Kato <paulkato460@yahoo.co.uk>
2025-04-02 11:06:41 +0300

    Checkout vercel build scripts from branch backencd

---
+++ backend/build_files.sh
@@ -0,0 +1,8 @@
+pip install -r requirements.txt
+
+python3.9 manage.py collectstatic --noinput
+
+# python3 ./manage.py makemigrations
+# python3 ./manage.py migrate
+
+# python3 ./manage.py runserver
