b7cb50879095d3cb9adef3c1eef675749d39412d
kyakumpi <2100900253@mubs.ac.ug>
2025-04-02 13:43:34 +0300

    Add solomons.model.py

--- backend/myproject/urls.py
+++
@@ -1,9 +0,0 @@
-# myproject/urls.py
-from django.contrib import admin
-from django.urls import path, include  # Import the 'include' function
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin URL
-    path('', include('task_app.urls')),  # Include URLs from task_app
-]
-
