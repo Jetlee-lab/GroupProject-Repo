c80a4eda09c592023288342d87eb753a3738c662
Keith Paul Kato <paulkato460@yahoo.co.uk>
2025-04-02 11:21:10 +0300

    Fix vercel build pip failed (no such command)

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,3 +1,12 @@
+echo " BUILD START..."
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv .venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+# install the required packages
 pip install -r requirements.txt
 
 python3.9 manage.py collectstatic --noinput
@@ -6,3 +15,10 @@ python3.9 manage.py collectstatic --noinput
 # python3 ./manage.py migrate
 
 # python3 ./manage.py runserver
+
+echo "...BUILD END"
+
+echo "SPINNING UP THE SERVER..."
+
+# [optional] Start the application here 
+python manage.py runserver
