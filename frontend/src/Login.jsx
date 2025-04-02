a22efb29746b4f3a8ae65802695d268c45eb2968
ASIIMWE ISAAC <asiimweisaac40@gmail.com>
2025-04-03 03:27:48 +1100

    Add axios

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:8000/api/login/", {
+      const response = await fetch("https://groupproject-repo.onrender.com/auth/browser/auth/login/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
