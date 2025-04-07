224848d2724d6c643db59ba368c910878787c183
Keith Paul Kato <paulkato460@yahoo.co.uk>
2025-04-07 18:19:05 +0300

    Build frontend

--- backend/build.sh
+++ backend/build.sh
@@ -6,26 +6,20 @@
 # print ----------+++++++++++++++++++++++++++++++++++++----------------------------
 
 
-# echo RUNNING MIGRATIONS...
+if [ ! -d "/app/backend/staticfiles" ]; then
+  
+    echo RUNNING MIGRATIONS...
+    python manage.py makemigrations
+    python manage.py migrate
 
-# python manage.py makemigrations
-# python manage.py migrate
+    echo POPULATING DATABASE...
+    python manage.py loaddata data
 
-# echo POPULATING DATABASE...
-
-# python manage.py loaddata d
-
-# # Collect static files
-# echo COLLECTING STATIC FILES...
-
-# # python manage.py collectstatic --noinput
-# # mkdir -p /app/backend/staticfiles
-# ls /app/backend/
-
-# # mv /app/frontend/dist /app/backend/staticfiles/frontend
-
-# echo =======================================================
-# ls /app/backend/staticfiles/frontend/
+    # Collect static files
+    echo COLLECTING STATIC FILES...
+    python manage.py collectstatic --noinput
+    # mv /app/frontend/dist /app/backend/staticfiles/frontend
+fi
 
 echo SPINING UP SERVER...
 
