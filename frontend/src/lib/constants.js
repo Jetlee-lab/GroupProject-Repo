f671ebca5f634a4d84fd8da4fb794ef18b4ab479
Keith Paul Kato <paulkato460@yahoo.co.uk>
2025-04-09 16:36:17 +0300

    Build frontend

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,5 +1,12 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
-// export const BACKEND_URL = 'https://groupproject-repo.onrender.com'
+let BACKEND_URL;
+
+if (import.meta.env.DEV) {
+  BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+} else {
+  BACKEND_URL = "https://groupproject-repo.onrender.com";
+}
+
+export { BACKEND_URL }
 export const API_URL = `${BACKEND_URL}/api/v1`;
 
 const STATUS_OPEN = "open";
@@ -9,12 +16,11 @@ const STATUS_RESOLVED = "resolved";
 const STATUS_REJECTED = "rejected";
 const STATUS_CLOSED = "closed";
 
-
 export {
-    STATUS_OPEN,
-    STATUS_INREVIEW,
-    STATUS_ESCALATED,
-    STATUS_RESOLVED,
-    STATUS_REJECTED,
-    STATUS_CLOSED,
-}
+  STATUS_OPEN,
+  STATUS_INREVIEW,
+  STATUS_ESCALATED,
+  STATUS_RESOLVED,
+  STATUS_REJECTED,
+  STATUS_CLOSED,
+};
