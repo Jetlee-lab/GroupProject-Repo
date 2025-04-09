037abeafc9106edb3bad1181a909704c535cb76a
Akandwanaho Jonan <akandwanahojonan256@gmail.com>
2025-04-09 22:26:09 +0300

    Update Dashboard.jsx
    updataed the dashbaord

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -129,7 +129,7 @@ export function DashboardLayout() {
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-5 p-5 pt-0">
           {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
