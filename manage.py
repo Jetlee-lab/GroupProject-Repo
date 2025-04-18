62b7f83f8339f83c8ae4e0a2cafb095fd3dc59bd
kyakumpi <2100900253@mubs.ac.ug>
2025-04-18 17:02:51 +0300

    Fix merge conflict in manage.py and cleaned setup

--- manage.py
+++ manage.py
@@ -6,11 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-=======
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kiyingis_pro.settings')
->>>>>>> 23ab9c35 (Set up project with Django Rest Framework and added initial configurations)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
