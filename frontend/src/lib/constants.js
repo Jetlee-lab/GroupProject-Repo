dd997549f3db3923eb71a3a2ceea2fca216e15c0
Keith Paul Kato <paulkato460@yahoo.co.uk>
2025-05-06 23:58:36 +0300

    add heroku build

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -15,11 +15,10 @@ let BACKEND_URL;
 if (import.meta.env.DEV) {
   BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
 } else {
-  BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  // https://groupproject-repo.onrender.com
+  // https://aits-groups-d962bd2c1d87.herokuapp.com
 
-  if (!BACKEND_URL) {
-    throw new Error("VITE_BACKEND_URL environment variable is not defined");
-  }
+  BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://aits-groups-d962bd2c1d87.herokuapp.com";
 }
 
 export { BACKEND_URL };
